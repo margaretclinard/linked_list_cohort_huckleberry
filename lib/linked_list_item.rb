50bc01a9776404d83915d474367c884daaa33c13
Margaret Clinard <margaret.clinard@gmail.com>
2015-04-13 16:10:39 -0500

    test_02b passing

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -5,4 +5,13 @@ class LinkedListItem
   def initialize(payload)
     @payload = payload
   end
+
+  attr_reader :next_item
+  def next_item=(next_item)
+    if next_item == self
+      raise ArgumentError
+    else
+      @next_item = next_item
+    end
+  end
 end
