9bfd504c1e16b334c848345d98f1ce9291fea669
Margaret Clinard <margaret.clinard@gmail.com>
2015-04-16 10:42:12 -0500

    Refactoring code and added initialize function with size

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,31 +3,29 @@ require_relative 'linked_list_item'
 class LinkedList
 
   attr_reader :first_item
-  #def size
-    #@size = 0
-  #end
+  def initialize
+    @size = 0
+  end
 
-  def push(item)
+  def push(payload)
     if @first_item.nil?
-      @first_item = LinkedListItem.new(item)
+      @first_item = LinkedListItem.new(payload)
       @last_item = @first_item
     else
-      @last_item.next_item = LinkedListItem.new(item)
+      @last_item.next_item = LinkedListItem.new(payload)
       @last_item = @last_item.next_item
     end
+    @size += 1
   end
 
   def get(index)
-    raise IndexError if index < 0 || @first_item == nil
+    raise IndexError if index < 0 || index > @size
     if index == 0
       @first_item.payload
     else
-      count = 0
       current_node = @first_item
-      while count < index
-      # above is the same as while count <= index - 1
+      index.times do
         current_node = current_node.next_item
-        count = count + 1
       end
       current_node.payload
     end
